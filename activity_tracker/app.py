ae11538658270743e1307385d759212e76c81782
lona9 <pilar.vasquez.h@gmail.com>
2022-11-09 18:03:33 -0300

    add new features

--- activity_tracker/app.py
+++ activity_tracker/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, g, request
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -20,13 +21,51 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('home.html')
+    db = get_db()
 
-@app.route('/view')
-def view():
-    return render_template('day.html')
+    if request.method == 'POST':
+        date = request.form['date']
+
+        dt = datetime.strptime(date, '%Y-%m-%d')
+        database_date = datetime.strftime(dt, '%Y%m%d')
+
+        db.execute('insert into log_date (entry_date) values (?)', [database_date])
+        db.commit()
+
+    cur = db.execute('select entry_date from log_date order by entry_date desc')
+    results = cur.fetchall()
+
+    pretty_results = []
+
+    for i in results:
+        single_date = {}
+        d = datetime.strptime(str(i['entry_date']), '%Y%m%d')
+        single_date['entry_date'] = datetime.strftime(d, '%B %d, %Y')
+
+        pretty_results.append(single_date)
+
+    return render_template('home.html', results=pretty_results)
+
+@app.route('/view/<date>', methods=['GET', 'POST'])
+def view(date):
+    if request.method == 'POST':
+        return ''
+
+    db = get_db()
+
+    cur = db.execute('select entry_date from log_date where entry_date = ?', [date])
+    result = cur.fetchone()
+
+    d = datetime.strptime(str(result['entry_date']), '%Y%m%d')
+    pretty_date = datetime.strftime(d, '%B %d, %Y')
+
+    activity_cur = db.execute('select id, name from activities')
+
+    activity_results = activity_cur.fetchall()
+
+    return render_template('day.html', date = pretty_date, activity_results = activity_results)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -38,12 +77,12 @@ def add():
         time = float(request.form['time'])
         distance = float(request.form['distance'])
         calories = float(request.form['calories'])
+        date = request.form['date']
 
-
-        db.execute('insert into activities (name, time, distance, calories) values (?,?,?,?)',
-        [name, time, distance, calories])
+        db.execute('insert into activities (name, time, distance, calories, date) values (?,?,?,?,?)',
+        [name, time, distance, calories, date])
         db.commit()
-    cur = db.execute('select name, time, distance, calories from activities')
+    cur = db.execute('select name, time, distance, calories, date from activities')
     results = cur.fetchall()
 
     return render_template('add.html', results=results)
